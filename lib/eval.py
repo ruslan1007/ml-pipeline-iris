c14fbec56391a5570332320f73c05e803120f46e
ruslan1007 <nagimov10@mail.ru>
2023-09-13 23:43:56 +0300

    [ADD]Added .py script part

---
+++ lib/eval.py
@@ -0,0 +1,42 @@
+import os
+import pickle
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import yaml
+
+from lib.train import METRICS, load_dict, save_dict
+
+def eval():
+    with open('params.yaml', 'r') as f:
+        params_data = yaml.safe_load(f)
+
+    config = params_data['eval']
+    with open('data/train/model.pkl', 'rb') as f:
+        model = pickle.load(f)
+
+    data = load_dict('data/train/data.json')
+    preds = model.predict(data['test_x'])
+
+    if not os.path.exists('data/eval'):
+        os.mkdir('data/eval')
+
+    metrics = {}
+    for metric_name in config['metrics']:
+        metrics[metric_name] = METRICS[metric_name](data['test_y'], preds)
+
+    save_dict(metrics, 'data/eval/metrics.json')
+
+    sns.heatmap(pd.DataFrame(data['test_x']).corr())
+    plt.savefig('data/eval/heatmap.png')
+
+    params = {'run_type': 'eval'}
+    for i in params_data.values():
+        params.update(i)
+
+    print(f'eval params - {params}')
+    print(f'eval metrics - {metrics}')
+
+if __name__ == '__main__':
+    eval()
---
+++ lib/train.py
@@ -0,0 +1,98 @@
+import json
+import os
+import pickle
+import random
+from functools import partial
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import yaml
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+
+
+RANDOM_SEED = 1
+
+random.seed(RANDOM_SEED)
+np.random.seed(RANDOM_SEED)
+
+METRICS = {
+    'recall': partial(recall_score, average='macro'),
+    'precision': partial(precision_score, average='macro'),
+    'accuracy': accuracy_score,
+}
+
+
+def save_dict(data: dict, filename: str):
+    with open(filename, 'w') as f:
+        json.dump(data, f)
+
+
+def load_dict(filename: str):
+    with open(filename, 'r') as f:
+        return json.load(f)
+
+
+def train_model(x, y):
+    model = DecisionTreeClassifier()
+    model.fit(x, y)
+    return model
+
+
+def train():
+    with open('params.yaml', 'rb') as f:
+        params_data = yaml.safe_load(f)
+
+    config = params_data['train']
+
+    iris = datasets.load_iris()
+    
+    task_dir = 'data/train'
+
+    x = iris['data'].tolist()
+    y = iris['target'].tolist()
+
+    train_x, test_x, train_y, test_y = train_test_split(x, y, test_size=config['test_size'])
+
+    model = train_model(train_x, train_y)
+
+    preds = model.predict(x)
+
+    metrics = {}
+    for metric_name in params_data['eval']['metrics']:
+        metrics[metric_name] = METRICS[metric_name](y, preds)
+
+    save_data = {
+        'train_x': train_x,
+        'test_x': test_x,
+        'train_y': train_y,
+        'test_y': test_y,
+    }
+
+    if not os.path.exists(task_dir):
+        os.makedirs(task_dir)
+
+    save_dict(save_data, os.path.join(task_dir, 'data.json'))
+    save_dict(metrics, os.path.join(task_dir, 'metrics.json'))
+
+    sns.heatmap(pd.DataFrame(train_x).corr())
+
+    plt.savefig('data/train/heatmap.png')
+
+    with open('data/train/model.pkl', 'wb') as f:
+        pickle.dump(model, f)
+
+    params = {'run_type': 'train'}
+    for i in params_data.values():
+        params.update(i)
+
+    print(f'train params - {params}')
+    print(f'train metrics - {metrics}')
+
+
+if __name__ == '__main__':
+    train()
